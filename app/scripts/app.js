eb855d60204b59aa569678dbc820221dd685fb1f
dhaval.patel <dhaval.patel@ishisystems.com>
2015-07-26 12:27:27 +0530

    Solves a bug.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,13 @@
 define(['exports', 'angularAMD', 'angular-route'], function (exports, _angularAMD, _angularRoute) {
+  /**
+   * @ngdoc overview
+   * @name angularAmdes6App
+   * @description
+   * # angularAmdes6App
+   *
+   * Main module of the application.
+   */
+
   'use strict';
 
   Object.defineProperty(exports, '__esModule', {
@@ -7,52 +16,21 @@ define(['exports', 'angularAMD', 'angular-route'], function (exports, _angularAM
 
   var angularApp = angular.module('angularAMDES6', ['ngRoute']).config(function ($routeProvider) {
     $routeProvider.when('/', _angularAMD.route({
-      templateUrl: 'views/main.html',
-      controllerUrl: 'controllers/main',
-      controller: 'MainCtrl',
+      templateUrl: 'views/todoCreate.html',
+      controllerUrl: 'controllers/todoCreate',
+      controller: 'TodoCreateCtrl',
       controllerAs: 'vm'
-    })).when('/about', _angularAMD.route({
-      templateUrl: 'views/about.html',
-      controllerUrl: 'controllers/about',
-      controller: 'AboutCtrl'
+    })).when('/list', _angularAMD.route({
+      templateUrl: 'views/todoList.html',
+      controllerUrl: 'controllers/todoList',
+      controller: 'TodoListCtrl',
+      controllerAs: 'vm'
     })).otherwise({
       redirectTo: '/'
     });
   });
 
   var app = _angularAMD.bootstrap(angularApp, document.body);
-  // define(['angularAMD', 'angular', 'angular-route'], function (angularAMD) {
-  //   var app = angular
-  //     .module('angularAmddemoApp', [
-  //       'ngRoute'
-  //     ])
-  //     .config(function ($routeProvider) {
-  //       $routeProvider
-  //         .when('/', angularAMD.route({
-  //           templateUrl: 'views/main.html',
-  //           controllerUrl: 'controllers/main',
-  //           controller: 'MainCtrl'
-  //         }))
-  //         .when('/about', angularAMD.route({
-  //           templateUrl: 'views/about.html',
-  //           controllerUrl: 'controllers/about',
-  //           controller: 'AboutCtrl'
-  //         }))
-  //         .otherwise({
-  //           redirectTo: '/'
-  //         });
-  //     });
-
-  //     return angularAMD.bootstrap(app, document.body);
-  //  });
   exports.app = app;
 });
-/**
- * @ngdoc overview
- * @name angularAmdes6App
- * @description
- * # angularAmdes6App
- *
- * Main module of the application.
- */
 //# sourceMappingURL=app.js.map
